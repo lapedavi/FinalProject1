dff80010f823af84b026c5afc9a98ec55242e981
David Lape <lapedavi@isu.edu>
2024-12-03 16:40:21 -0600

    Added load and save functionality

--- gui.py
+++ gui.py
@@ -4,13 +4,16 @@ class Gui:
     def __init__(self, window):
         self.window = window
 
+        # Main Menu
         self.main_frame = Frame(self.window)
 
         self.vote_menu_label = Label(self.main_frame, text="VOTE MENU", font=("Helvetica", 10))
         self.vote_button = Button(self.main_frame, text='Vote')
+        self.load_button = Button(self.main_frame, text='Load')
         self.exit_button = Button(self.main_frame, text='Exit')
         self.vote_menu_label.pack(pady=5)
-        self.vote_button.pack(pady=10)
+        self.vote_button.pack(pady=5)
+        self.load_button.pack(pady=5)
         self.exit_button.pack()
 
         self.vote_result_label  = Label(self.main_frame, font=("Helvetica", 10))
@@ -18,6 +21,7 @@ class Gui:
 
         self.main_frame.pack()
 
+        # Vote Form
         self.vote_frame = Frame(self.window)
         self.candidate_menu_label = Label(self.vote_frame, text="CANDIDATE MENU", font=("Helvetica", 10))
         self.candidate_menu_label.pack(pady=10)
@@ -38,7 +42,13 @@ class Gui:
         self.error_label = Label(self.vote_frame, font=("Helvetica", 10))
         self.error_label.pack(pady=20)
 
-        self.final_label = Label(self.window, font=("Helvetica", 10))
+        # Final Frame
+        self.result_frame = Frame(self.window)
+
+        self.result_label = Label(self.result_frame, font=("Helvetica", 10))
+        self.save_button = Button(self.result_frame, text='Save Results')
+        self.result_label.pack(pady=10)
+        self.save_button.pack()
 
 
 
--- logic.py
+++ logic.py
@@ -1,6 +1,10 @@
+import os.path
+
 from gui import *
 
 class Logic(Gui):
+    FILE_NAME = 'data.csv'
+
     def __init__(self, window) -> None:
         super().__init__(window)
 
@@ -10,14 +14,16 @@ class Logic(Gui):
         self.exit_button.config(command=self.select_exit)
         self.vote_button.config(command=self.open_candidate_menu)
         self.cast_button.config(command=self.cast_vote)
+        self.load_button.config(command=self.load_votes)
+        self.save_button.config(command=self.save_votes)
 
 
     def select_exit(self) -> None:
         """Closes both vote and main screens and displays votes per candidate and total votes"""
         self.vote_frame.pack_forget()
         self.main_frame.pack_forget()
-        self.final_label.config(text=f'John - {self.__john_votes}, Jane - {self.__jane_votes}, Total - {self.__john_votes + self.__jane_votes}')
-        self.final_label.pack(pady=20)
+        self.result_label.config(text=f'John - {self.__john_votes}, Jane - {self.__jane_votes}, Total - {self.__john_votes + self.__jane_votes}')
+        self.result_frame.pack()
 
     def cast_vote(self) -> None:
         """Add vote to selected candidate, if vote was not provided return an error message.
@@ -45,3 +51,27 @@ class Logic(Gui):
         """Closes vote form and opens main menu"""
         self.vote_frame.pack_forget()
         self.main_frame.pack()
+
+    def load_votes(self) -> None:
+        """Read data file to load the previously saved votes. If data file does not exist show an error"""
+        if os.path.isfile(Logic.FILE_NAME):
+            data_file = open(Logic.FILE_NAME,'r')
+        else:
+            self.vote_result_label.config(text='File does not exist')
+            return
+
+        line_data = data_file.readline()
+        data_file.close()
+
+        split_data = line_data.split(',')
+        self.__john_votes = int(split_data[0])
+        self.__jane_votes = int(split_data[1])
+        self.vote_result_label.config(text='Read data successfully')
+
+    def save_votes(self) -> None:
+        """Write current vote counts to data file"""
+        data_file = open(Logic.FILE_NAME,'w')
+        data_file.write(f'{self.__john_votes},{self.__jane_votes}')
+        data_file.close()
+        self.save_button.pack_forget()
+        self.result_label.config(text=f'Votes saved successfully')
